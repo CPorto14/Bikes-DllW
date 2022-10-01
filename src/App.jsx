5fc8b114cf704fe8313372531397d3c8854c5f51
Cesar Porto <zprex14@gmail.com>
2022-10-01 00:04:51 -0500

    Pagina principal con las paradas,compañias y ciudades

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react"
-import {getParadas} from "./services/paradas"
+import { getParadas } from "./services/paradas"
+import logo from "./assets/Bici.png"
+import { Heading, Box, Image, Flex, Text, Spacer,Tag } from '@chakra-ui/react'
 
 
 export function App() {
@@ -13,15 +15,34 @@ export function App() {
 
     return (
         <>
-        <h1>Paradas</h1>
-        <ul>
-            {paradas.map((parada) =>(
-                <li key={parada.id}>
-                    {parada.company}
-                </li>
+            <center><Image m={4} src={logo} width={200} /></center>
+            <Heading as="h1" size="lg" m={4}>
+                Paradas
+            </Heading>
+            <section>
+                {paradas.map((parada) => (
+                    <Box
+                        key={parada.id}
+                        bg="blue.100"
+                        p={4}
+                        m={4} borderRadius="lg"
+                    >
+                        <Flex>
+                            <Text fontSize="2x1">
+                                <strong>Nombre de la parada: {parada.name} </strong> <br />
+                                - Compañia: {parada.company}  <br />
+                                - Ciudad: {parada.location.city}, {parada.location.country}
+                            </Text>
+                            <Spacer />
+                            <Tag p={4} colorScheme="green">
+                                
+                            </Tag>
+                        </Flex>
 
-            ))}
-        </ul>
+                    </Box>
+
+                ))}
+            </section>
         </>
     )
 }
